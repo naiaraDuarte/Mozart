7a12a9a5d04212f7b277c5145659b3a31de856df
naiaraDuarte <naiara.duarte123@gmail.com>
2020-12-03 17:16:09 -0300

    novo login

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+
+$("#logar").click(function(){
+    var nomeUsuario = document.getElementById("nomeusuario").value;
+    var senha = document.getElementById("senha").value;
+    if(nomeUsuario == "admin" && senha == "1234"){
+        $(location).attr('href', 'home.html');
+    }else{
+        alert("Login e senha incorreto");
+    }
+});
